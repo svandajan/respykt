476356b3783520753230f6d1bb799a8685be8488
Jan Švanda <jan.svanda.volny@gmail.com>
2019-03-11 17:56:37 +0100

    changing license to "European Union Public Licence 1.2" (EUPL 1.2) (GPL compatible, OSI Approved)

--- setup.py
+++ setup.py
@@ -10,11 +10,20 @@ setup(
     name="respykt",
     version="0.8",
     description="A useful module",
-    license="GPL v3",
+    license="EUPL 1.2",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Jan Svanda",
     author_email="svandajan@svandajan.cz",
     url="http://python.svandajan.cz/",
     packages=["respykt"],
-    install_requires=["requests", "beautifulsoup4", "Mako"]
+    package_data={"respykt": ["resources/*"]},
+    install_requires=["requests", "beautifulsoup4", "Mako"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: European Union Public Licence 1.2 (EUPL 1.2)",
+
+    ]
 )
