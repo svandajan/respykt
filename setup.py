605112d2877d0e1b683d8391932f4f27360e92d4
Jan Švanda <jan.svanda.volny@gmail.com>
2019-03-11 17:29:51 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="respykt",
+    version="0.8",
+    description="A useful module",
+    license="GPL v3",
+    long_description=long_description,
+    author="Jan Svanda",
+    author_email="svandajan@svandajan.cz",
+    url="http://python.svandajan.cz/",
+    packages=["respykt"],
+    install_requires=["requests", "beautifulsoup4", "Mako"]
+)
